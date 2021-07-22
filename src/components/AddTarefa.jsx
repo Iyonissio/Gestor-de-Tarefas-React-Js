9d9de2cff518dc9ea10e58aacbf27ba6b3fde97a
Iyonissio Daniel Sitoe <57901563+Iyonissio@users.noreply.github.com>
2021-07-22 23:06:48 +0200

    2 commit

--- src/components/AddTarefa.jsx
+++ src/components/AddTarefa.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
+import Button from './Button';
+
 import "./AddTarefa.css";
 
 const AddTarefa = () => {
     return (
         <div className="add-tarefa-container">
             <input className="add-tarefa-input" type="text" />
+            <div className="add-tarefa-button-container">
+                <Button>Adicionar</Button>
+            </div>
         </div>
-        
     );
 }
 
